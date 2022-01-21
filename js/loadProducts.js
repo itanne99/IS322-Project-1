50586b529f9d44374bf0beac28017b54461bf6ab
Ido Tanne <idovader@gmail.com>
2022-01-21 13:46:57 -0500

    implemented page to load all products

---
+++ js/loadProducts.js
@@ -0,0 +1,20 @@
+// const urlSearchParams = new URLSearchParams(window.location.search);
+// const params = Object.fromEntries(urlSearchParams.entries());
+
+fetch('./json/product-dataset.json')
+    .then(jsonData => jsonData.json())
+    .then(data => printIt(data))
+
+let printIt = (data) => {
+    try{
+        data.forEach(item => {
+            document.getElementById("product-list").innerHTML += `<div class="row" id=${item.uniq_id}> <a href="product.html?id=${item.uniq_id}">${item.product_name}</a>  </div>`;
+        });
+        // document.getElementById("content").innerHTML += "<div>"+
+        //     "Hello I am "+item.first_name+" "+item.last_name
+        //     +"</div>";
+        // console.log(item);
+    } catch (e) {
+
+    }
+}
