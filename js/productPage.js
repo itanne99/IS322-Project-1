c3e003de84a2bf4bb756e311f5dfe5d5cba5314f
Ido Tanne <idovader@gmail.com>
2022-01-21 17:22:09 -0500

    implemented a sufficient back-end for product detail page

---
+++ js/productPage.js
@@ -0,0 +1,27 @@
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+
+fetch('./json/product-dataset.json')
+    .then(jsonData => jsonData.json())
+    .then(data => printIt(data))
+
+let printIt = (data) => {
+    try{
+        let item = data[parseInt(params.id)]
+        //changes title to product name
+        document.title = item.product_name;
+        //loads the img
+        document.getElementById("productImg").src=item.image.split('|')[0];
+        //load product details
+        document.getElementById("productTitle").innerHTML=item.product_name;
+        document.getElementById("productModelID").innerHTML+=item.model_number;
+        document.getElementById("productDescription").innerHTML=item.about_product;
+        document.getElementById("productPrice").innerHTML=item.selling_price;
+    } catch (e) {
+        if(params.id === undefined){
+            document.getElementById("productTitle").innerHTML = "ID not defined";
+        } else {
+            document.getElementById("productTitle").innerHTML = "ID not found";
+        }
+    }
+}
