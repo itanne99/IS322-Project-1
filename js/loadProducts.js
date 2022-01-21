14f6ad36aff410e9cae07c27f5b1c3624406afe7
Ido Tanne <idovader@gmail.com>
2022-01-21 17:21:23 -0500

    added random item selector and adjusted name for product detail page

--- js/loadProducts.js
+++ js/loadProducts.js
@@ -1,14 +1,22 @@
 // const urlSearchParams = new URLSearchParams(window.location.search);
 // const params = Object.fromEntries(urlSearchParams.entries());
 
+
+//pulled from https://www.w3schools.com/js/js_random.asp
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
+
 fetch('./json/product-dataset.json')
     .then(jsonData => jsonData.json())
     .then(data => printIt(data))
 
 let printIt = (data) => {
     try{
+
+        document.getElementById("productList").innerHTML += `<div class="row mb-3" id=${getRndInteger(0,data.length)}> <h4><a href="productPage.html?id=${getRndInteger(0,data.length)}">Look Up Random Item</a></h4></div>`
         data.forEach((item, index) => {
-            document.getElementById("productList").innerHTML += `<div class="row" id=${index}> <a href="product.html?id=${index}">${item.product_name}</a>  </div>`;
+            document.getElementById("productList").innerHTML += `<div class="row" id=${index}> <a href="productPage.html?id=${index}">${item.product_name}</a>  </div>`;
         });
         // document.getElementById("content").innerHTML += "<div>"+
         //     "Hello I am "+item.first_name+" "+item.last_name
